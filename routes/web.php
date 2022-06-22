fcfc2a09422a9c9ba620f4d23101df9a58702a70
ChiaraInMiniatura <chiarag295@gmail.com>
2022-06-22 19:43:53 +0200

    consegna esercizio

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'user' => 'Ugo'
+    ];
+
+    return view('home', $data);
+});
+Route::get('/pagina1', function () {
+
+    return view('pagina1');
+});
+Route::get('/pagina2', function () {
+
+    return view('pagina2');
 });
